f0c4c9a5ae9882f2de5b4999720b7a2ac00c0c31
treecoww <tcao89@gmail.com>
2023-04-07 15:48:37 -0500

    define columns for product tagss

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -16,5 +16,48 @@ ProductTag.init(
     modelName: 'product_tag',
   }
 );
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    // define columns
+    //produtTag id: integer, doesn't allow null values, set as primary key, uses auto increment
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    //product_id: integer, references the product model's id
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    //tag_id: integer, references the tag model's id
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
 
 module.exports = ProductTag;
