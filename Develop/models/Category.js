66f3b569794dbcb02eb65587d0c13b35b2f32346
treecoww <tcao89@gmail.com>
2023-04-07 15:36:39 -0500

    define columns for id & category

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,7 +6,19 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // define columns: id & category
+    //id (integer, doesn't allow null values, set as primary key, uses auto increment)
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    //category_name (string, doesn't allow null values)
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
