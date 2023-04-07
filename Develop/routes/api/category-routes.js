2590447021597b4d02fc9ea8ef9098e7c80256a0
treecoww <tcao89@gmail.com>
2023-04-07 17:21:13 -0500

    update category api

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -48,8 +48,14 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body,{where:{id:params.id}})
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
